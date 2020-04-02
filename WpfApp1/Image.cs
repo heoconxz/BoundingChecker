ee0080df001bb9ffcbe024ce8645d75e6a5e3226
Tung Tran <heoconxz@live.com>
2020-04-02 23:22:22 +0700

    Add model

---
+++ WpfApp1/Image.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace BoundingChecker
+{
+    public class BoundingImage
+    {
+        public string Path { get; set; }
+        public XmlDocument XmlIn { get; set; }
+        public XmlDocument XmlOut { get; set; }
+
+        public Result GetResult()
+        {
+            if (XmlIn == null || XmlOut == null)
+            {
+                return new Result();
+            }
+
+            return new Result();
+        }
+    }
+
+    public class Result
+    {
+        private int Total { get; set; }
+        private int Correct { get; set; }
+        private int Miss { get; set; }
+        private int Wrong { get; set; }
+
+        public Result(int t = 0, int c = 0, int m = 0, int w = 0)
+        {
+            Total = t;
+            Correct = c;
+            Miss = m;
+            Wrong = w;
+        }
+    }
+}
--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -12,17 +12,77 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Xml;
 
-namespace WpfApp1
+namespace BoundingChecker
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
+        private int frame;
+
+        public int Frame
+        {
+            get
+            {
+                return frame;
+            }
+            set
+            {
+                frame = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public int Offset { get; set; }
+
+        public List<BoundingImage> Images { get; set; }
+
         public MainWindow()
         {
+            Frame = 1;
+            Offset = 10;
             InitializeComponent();
+            LoadData(null, null);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void LoadData(object sender, RoutedEventArgs e)
+        {
+            Images = new List<BoundingImage>();
+            foreach (string filePath in Directory.EnumerateFiles(@".\Image", "*.png"))
+            {
+                var boundingImage = new BoundingImage()
+                {
+                    Path = filePath,
+                    XmlIn = new XmlDocument(),
+                    XmlOut = new XmlDocument()
+                };
+                boundingImage.XmlIn.Load(System.IO.Path.Combine(@".\Input", System.IO.Path.GetFileNameWithoutExtension(filePath) + ".xml"));
+                boundingImage.XmlOut.Load(System.IO.Path.Combine(@".\Output", System.IO.Path.GetFileNameWithoutExtension(filePath) + ".xml"));
+                Images.Add(boundingImage);
+            }
+        }
+
+        private void NextFrame(object sender, RoutedEventArgs e)
+        {
+            Frame++;
+        }
+
+        private void PrevFrame(object sender, RoutedEventArgs e)
+        {
+            Frame--;
+        }
+
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
         }
     }
 }
